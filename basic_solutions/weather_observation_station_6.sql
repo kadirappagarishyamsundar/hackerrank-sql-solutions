be38d2fccfce5cfa10abc537800ac255da9d2889
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-05 21:51:28 +0530

    Added 2 basic solutions

---
+++ basic_solutions/weather_observation_station_6.sql
@@ -0,0 +1,10 @@
+ #problem: Weather Observation Station 6
+ #https://www.hackerrank.com/challenges/weather-observation-station-6/problem
+
+SELECT DISTINCT city 
+FROM station
+WHERE city LIKE 'a%'
+   OR city LIKE 'e%'
+   OR city LIKE 'i%'
+   OR city LIKE 'o%'
+   OR city LIKE 'u%';
---
+++ basic_solutions/weather_observation_station_7.sql
@@ -0,0 +1,10 @@
+#problem:Weather Observation Station 7
+#https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+SELECT DISTINCT city
+FROM station
+WHERE city LIKE '%a'
+   OR city LIKE '%e'
+   OR city LIKE '%i'
+   OR city LIKE '%o'
+   OR city LIKE '%u';
