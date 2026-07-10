9a971f8619ca629d4158f9287e84575606157d0e
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-10 14:44:14 +0530

    Added 2 more basic solutions

---
+++ basic_solutions/employee_salaries.sql
@@ -0,0 +1,8 @@
+#problem:Employee Salaries
+#https://www.hackerrank.com/challenges/salary-of-employees/problem
+
+SELECT name
+FROM Employee
+WHERE salary > 2000 and months < 10
+ORDER BY employee_id ASC;
+
---
+++ basic_solutions/weather_observation_station_16.sql
@@ -0,0 +1,7 @@
+#problem:Weather Observation Station 16
+#https://www.hackerrank.com/challenges/weather-observation-station-16/problem
+
+SELECT ROUND(MIN(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7780;
+
