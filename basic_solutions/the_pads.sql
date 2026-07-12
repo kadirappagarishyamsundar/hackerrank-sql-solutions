ca04823fbecddad394e84348ae1724e2f372c5a0
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-12 22:47:29 +0530

    solved two basic solutions

---
+++ basic_solutions/the_pads.sql
@@ -0,0 +1,12 @@
+#problem:The PADS
+#https://www.hackerrank.com/challenges/the-pads/problem
+
+
+SELECT CONCAT(Name,'(',LEFT(Occupation,1),')')
+FROM OCCUPATIONS
+ORDER BY Name ASC;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ',LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation) ASC, Occupation ASC;
