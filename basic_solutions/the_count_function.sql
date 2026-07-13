8c3ea3b49e9816473dc257be77e9162303db5a55
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-13 23:32:54 +0530

    added count,sum function solutions

---
+++ basic_solutions/the_count_function.sql
@@ -0,0 +1,6 @@
+#problem:Revising Aggregations - The Count Function
+#https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+
+SELECT COUNT(DISTRICT)
+FROM CITY
+WHERE POPULATION >100000;
---
+++ basic_solutions/the_sum_function.sql
@@ -0,0 +1,6 @@
+#problem:Revising Aggregations - The Sum Function
+#https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California'
