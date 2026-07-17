dfda4799b7a24a999d28f87fdf19a7da8937b284
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-17 22:35:58 +0530

    added three basic solutions

---
+++ basic_solutions/weather_observation_sttation_14.sql
@@ -0,0 +1,10 @@
+#problem:Weather Observation Station 14
+#https://www.hackerrank.com/challenges/weather-observation-station-14/problem
+
+SELECT (ROUND(MAX(LAT_N),4))
+FROM STATION
+WHERE LAT_N < 137.2345;
+
+
+
+
