bc8979beb7324eb62398a06f48e15c6087ccc7d9
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-19 11:23:46 +0530

    added two basic solutions

---
+++ basic_solutions/weather_observation_station_19.sql
@@ -0,0 +1,5 @@
+#problem:Weather Observation Station 19
+#https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+
+SELECT ROUND(SQRT(POW(MIN(LAT_N) - MAX(LAT_N),2) + POW(MIN(LONG_W) - MAX(LONG_W),2)),4)
+FROM STATION;
